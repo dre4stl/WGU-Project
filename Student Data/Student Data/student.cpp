06b8fa2776ab939d4a6d5ae14fac802f4f7fd186
dre4stl <dre4stl@gmail.com>
2020-07-03 16:50:00 -0400

    Add files via upload

---
+++ Student Data/Student Data/student.cpp
@@ -0,0 +1,12 @@
+//
+//  student.cpp
+//  Student Data
+//
+//  Created by DeAundra Dyson on 7/2/20.
+//  Copyright © 2020 DeAundra Dyson. All rights reserved.
+//
+
+#include "student.hpp"
+#include <iostream>
+#include <string>
+
---
+++ Student Data/Student Data/student.hpp
@@ -0,0 +1,14 @@
+//
+//  student.hpp
+//  Student Data
+//
+//  Created by DeAundra Dyson on 7/2/20.
+//  Copyright © 2020 DeAundra Dyson. All rights reserved.
+//
+
+#ifndef student_hpp
+#define student_hpp
+
+#include <stdio.h>
+
+#endif /* student_hpp */
